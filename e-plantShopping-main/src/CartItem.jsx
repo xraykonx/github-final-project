471767eb66a924ac6f0028f3b7d3a2014a408862
xraykonx <50393350+xraykonx@users.noreply.github.com>
2024-11-01 11:39:03 -0500

    Add files via upload

---
+++ e-plantShopping-main/src/CartItem.jsx
@@ -0,0 +1,118 @@
+import PropTypes from "prop-types";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  removeItem,
+  updateQuantity,
+} from "./CartSlice";
+import "./CartItem.css";
+
+const CartItem = ({ onContinueShopping }) => {
+  const cart = useSelector((state) => state.cart.items);
+  const dispatch = useDispatch();
+  const calculateTotalAmount = () => {
+    let totalCost = 0;
+    cart.forEach((item) => {
+      totalCost += item.cost.replace("$", "") * item.quantity;
+    });
+
+    return totalCost;
+  };
+
+  const handleContinueShopping = (e) => {
+    onContinueShopping(e);
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert("Functionality to be added for future reference");
+  };
+
+  const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
+  };
+
+  const handleDecrement = (item) => {
+    if (item.quantity > 1) {
+      dispatch(
+        updateQuantity({ name: item.name, quantity: item.quantity - 1 })
+      );
+    }
+  };
+
+  const handleRemove = (item) => {
+    dispatch(removeItem(item));
+  };
+
+  const calculateTotalCost = (item) => {
+    return item.cost.replace("$", "") * item.quantity;
+  };
+
+  return (
+    <div className="cart-container">
+      <h2 style={{ color: "black" }}>
+        Total Cart Amount: ${calculateTotalAmount()}
+      </h2>
+      <div>
+        {cart.map((item) => (
+          <div className="cart-item" key={item.name}>
+            <img className="cart-item-image" src={item.image} alt={item.name} />
+            <div className="cart-item-details">
+              <div className="cart-item-name">{item.name}</div>
+              <div className="cart-item-cost">{item.cost}</div>
+              <div className="cart-item-quantity">
+                <button
+                  className="cart-item-button cart-item-button-dec"
+                  onClick={() => handleDecrement(item)}
+                >
+                  -
+                </button>
+                <span className="cart-item-quantity-value">
+                  {item.quantity}
+                </span>
+                <button
+                  className="cart-item-button cart-item-button-inc"
+                  onClick={() => handleIncrement(item)}
+                >
+                  +
+                </button>
+              </div>
+              <div className="cart-item-total">
+                Total: ${calculateTotalCost(item)}
+              </div>
+              <button
+                className="cart-item-delete"
+                onClick={() => handleRemove(item)}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div
+        style={{ marginTop: "20px", color: "black" }}
+        className="total_cart_amount"
+      ></div>
+      <div className="continue_shopping_btn">
+        <button
+          className="get-started-button"
+          onClick={(e) => handleContinueShopping(e)}
+        >
+          Continue Shopping
+        </button>
+        <br />
+        <button
+          className="get-started-button1"
+          onClick={() => handleCheckoutShopping()}
+        >
+          Checkout
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
+
+CartItem.propTypes = {
+  onContinueShopping: PropTypes.func.isRequired,
+};
---
+++ e-plantShopping-main/src/CartSlice.jsx
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const CartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+    },
+    decrementItem: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) {
+        if (existingItem.quantity >= 2) {
+          existingItem.quantity--;
+        }
+      } else {
+        state.items.push({ name, image, cost, quantity: 0 });
+      }
+    },
+    removeItem: (state, action) => {
+      state.items = state.items.filter(
+        (item) => item.name !== action.payload.name
+      );
+    },
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const itemToUpdate = state.items.find((item) => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
+    },
+  },
+});
+
+export const { addItem, decrementItem, removeItem, updateQuantity } =
+  CartSlice.actions;
+
+export default CartSlice.reducer;
